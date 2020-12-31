939767c58534865576071a2ac1c4e62995c58306
Aejaz Habib <aejaz.h@outlook.com>
2020-12-31 12:19:20 +0530

    Changed File Name
    Signed-off-by: Aejaz Habib <aejaz.h@outlook.com>

--- SAP_CONNECTOR/Form1.Designer.cs
+++ SAP_CONNECTOR/Form1.Designer.cs
@@ -29,10 +29,17 @@ namespace SAP_CONNECTOR
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Form1";
+            this.Text = "SapCreds";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
